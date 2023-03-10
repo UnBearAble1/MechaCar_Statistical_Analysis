25caae9bc12c2fc2690c99074a7963c2a11da3eb
UnBearAble1 <115948081+UnBearAble1@users.noreply.github.com>
2023-03-10 14:57:03 -0600

    Delete MechaCarChallenge.R

--- MechaCarChallenge.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-
-MechaCar_mpg <- read.csv(file="MechaCar_mpg.csv")
-
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
