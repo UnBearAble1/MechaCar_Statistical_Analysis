7751e03268742a34c6e7b2c6e72d8eddf454715a
UnBearAble1 <115948081+UnBearAble1@users.noreply.github.com>
2023-03-10 12:00:43 -0600

    Add files via upload
    uploading updated files to fix multiple regression equation

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+MechaCar_mpg <- read.csv(file="MechaCar_mpg.csv")
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
